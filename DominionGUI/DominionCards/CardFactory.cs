817563aee2596becf3cffefb73df192585636576
Josh Gayso <gaysojj@rose-hulman.edu>
2016-03-27 16:52:01 -0400

    Created Factory that will be updated when decision making is completed.

---
+++ DominionGUI/DominionCards/CardFactory.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DominionCards.KingdomCards;
+
+namespace DominionCards
+{
+    class ActionCardFactory
+    {
+
+        public ActionCard createNewAdventurer()
+        {
+            return new Adventurer();
+        }
+
+        public ActionCard createNewCellar()
+        {
+            return new Cellar();
+        }
+
+        public ActionCard createNewChancellor()
+        {
+            return new Chancellor();
+        }
+
+        public ActionCard createNewChapel()
+        {
+            return new Chapel();
+        }
+
+        public ActionCard createNewCouncilRoom()
+        {
+            return new CouncilRoom();
+        }
+
+        public ActionCard createNewFeast()
+        {
+            return new Feast();
+        }
+
+        public ActionCard createNewChapel()
+        {
+            return new Chapel();
+        }
+
+        public ActionCard createNewCouncilRoom()
+        {
+            return new CouncilRoom();
+        }
+
+        public ActionCard createNewFeast()
+        {
+            return new Feast();
+        }
+
+        public ActionCard createNewFestival()
+        {
+            return new Festival();
+        }
+
+        public ActionCard createNewLaboratory()
+        {
+            return new Laboratory();
+        }
+
+        public ActionCard createNewLibrary()
+        {
+            return new Library();
+        }
+
+        public ActionCard createNewMarket()
+        {
+            return new Market();
+        }
+
+        public ActionCard createNewMine()
+        {
+            return new Mine();
+        }
+
+        public ActionCard createNewMoneyLender()
+        {
+            return new MoneyLender();
+        }
+
+        public ActionCard createNewLaboratory()
+        {
+            return new Laboratory();
+        }
+
+        public ActionCard createNewRemodel()
+        {
+            return new Remodel();
+        }
+
+        public ActionCard createNewSmithy()
+        {
+            return new Smithy();
+        }
+
+        public ActionCard createNewThroneRoom()
+        {
+            return new ThroneRoom();
+        }
+
+        public ActionCard createNewVillage()
+        {
+            return new Village();
+        }
+
+        public ActionCard createNewWoodCutter()
+        {
+            return new Woodcutter();
+        }
+
+        public ActionCard createNewWorkshop()
+        {
+            return new Workshop();
+        }
+    }
+}
