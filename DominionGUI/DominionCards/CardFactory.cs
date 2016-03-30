f343e484688d3ff5f0747ead9ddbc697c1747552
Josh Gayso <gaysojj@rose-hulman.edu>
2016-03-29 20:42:04 -0400

    Updated the Card Factory to create Action Cards using card specific parameters

--- DominionGUI/DominionCards/CardFactory.cs
+++ DominionGUI/DominionCards/CardFactory.cs
@@ -9,6 +9,20 @@ namespace DominionCards
 {
     class ActionCardFactory
     {
+        private static readonly int CELLAR_ID = 8;
+        private static readonly int CHANCELLOR_ID = 9;
+        private static readonly int CHAPEL_ID = 10;
+        private static readonly int FEAST_ID = 12;
+        private static readonly int FESTIVAL_ID = 13;
+        private static readonly int LABORATORY_ID = 15;
+        private static readonly int MINE_ID = 19;
+        private static readonly int MARKET_ID = 17;
+        private static readonly int REMODEL_ID = 22;
+        private static readonly int SMITHY_ID = 23;
+        private static readonly int THRONEROOM_ID = 26;
+        private static readonly int VILLAGE_ID = 28;
+        private static readonly int WOODCUTTER_ID = 30;
+        private static readonly int WORKSHOP_ID = 31;
 
         public ActionCard createNewAdventurer()
         {
@@ -17,17 +31,17 @@ namespace DominionCards
 
         public ActionCard createNewCellar()
         {
-            return new Cellar();
+            return new ActionCard("Cellar", 0, 0, 0, 1, 2, CELLAR_ID);
         }
 
         public ActionCard createNewChancellor()
         {
-            return new Chancellor();
+            return new ActionCard("Chancellor", 0, 2, 0, 0, 3, CHANCELLOR_ID);
         }
 
         public ActionCard createNewChapel()
         {
-            return new Chapel();
+            return new ActionCard("Chapel", 0, 0, 0, 0, 2, CHAPEL_ID);
         }
 
         public ActionCard createNewCouncilRoom()
@@ -37,17 +51,17 @@ namespace DominionCards
 
         public ActionCard createNewFeast()
         {
-            return new Feast();
+            return new ActionCard("Feast", 0, 0, 0, 0, 4, FEAST_ID);
         }
 
         public ActionCard createNewFestival()
         {
-            return new Festival();
+            return new ActionCard("Festival", 0, 2, 1, 2, 5, FESTIVAL_ID);
         }
 
         public ActionCard createNewLaboratory()
         {
-            return new Laboratory();
+            return new ActionCard("Laboratory", 2, 0, 0, 1, 5, LABORATORY_ID);
         }
 
         public ActionCard createNewLibrary()
@@ -57,12 +71,12 @@ namespace DominionCards
 
         public ActionCard createNewMarket()
         {
-            return new Market();
+            return new ActionCard("Market", 1, 1, 1, 1, 5, MARKET_ID);
         }
 
         public ActionCard createNewMine()
         {
-            return new Mine();
+            return new ActionCard("Mine", 0, 0, 0, 0, 5, MINE_ID);
         }
 
         public ActionCard createNewMoneyLender()
@@ -72,32 +86,32 @@ namespace DominionCards
 
         public ActionCard createNewRemodel()
         {
-            return new Remodel();
+            return new ActionCard("Remodel", 0, 0, 0, 0, 4, REMODEL_ID);
         }
 
         public ActionCard createNewSmithy()
         {
-            return new Smithy();
+            return new ActionCard("Smithy", 3, 0, 0, 0, 4, SMITHY_ID);
         }
 
         public ActionCard createNewThroneRoom()
         {
-            return new ThroneRoom();
+            return new ActionCard("Throne Room", 0, 0, 0, 0, 4, THRONEROOM_ID);
         }
 
         public ActionCard createNewVillage()
         {
-            return new Village();
+            return new ActionCard("Village", 1, 0, 0, 2, 3, VILLAGE_ID);
         }
 
         public ActionCard createNewWoodCutter()
         {
-            return new Woodcutter();
+            return new ActionCard("Wood cutter", 0, 2, 1, 0, 3, WOODCUTTER_ID);
         }
 
         public ActionCard createNewWorkshop()
         {
-            return new Workshop();
+            return new ActionCard("Workshop", 0, 0, 0, 0, 3, WORKSHOP_ID);
         }
     }
 }
